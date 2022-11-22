165b0964000371a80e1531d40cf6dc796e9b07a5
ThomasYoussef-Business <thomasyoussef.business@protonmail.com>
2022-11-22 16:24:56 +0100

    Definiti tutti i valori

--- Program.cs
+++ Program.cs
@@ -1,7 +1,26 @@
 ﻿namespace csharp_bookstore {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
+            string titolo, autore, isbn;
+            int pagine, peso, numeroRecensioni;
+            float altezza, larghezza, profondita, valutazioneMedia;
+            bool esisteKindle, esisteRigido;
+
+            titolo = "Clean Code: A Handbook of Agile Software Craftsmanship";
+            autore = "Robert C. Martin";
+            isbn = "978-0132350884";
+            pagine = 431;
+            peso = 800;
+            altezza = 23.37f;
+            larghezza = 17.78f;
+            profondita = 2.79f;
+            valutazioneMedia = 4.7f;
+            esisteKindle = true;
+            esisteRigido = true;
+
+
+
+
         }
     }
 }
